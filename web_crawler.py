bc5a1b9a88213e010ebaf1444c1ebf1028c23c91
hellobasti <bastianwinterott@gmail.com>
2016-11-08 20:41:50 +0100

    Added function

--- web_crawler.py
+++ web_crawler.py
@@ -1,32 +1,33 @@
-# Select a website
-# Start request
-# Handle the response
-
 # constants
-const_link_search = '<a href='
+const_html_tag = '<a href='
+
+def get_next_url(page):
+
+    start_html_tag = page.find(const_html_tag)
+
+    start_url = page.find('"', start_html_tag)
+
+    end_url = page.find('"', start_url+1)
+
+    url = page[start_url+1:end_url]
+
+    return url, end_url
+
+# Select a url for a website
+# Request
+# Handle the response --> result page
 
 page = ('<div id="top_bin"> <div id="top_content" class="width960"> <div class="udacity float-left"> <a href="http://udacity.com">Hello world</a>')
 
-# find the position for the first link (<a href=)
-start_link = page.find(const_link_search)
-
-# logging
-print ('start_link: ', start_link)
-
-start_url = page.find('"', start_link)
-
-# logging
-print ('start_url: ', start_url)
-
-end_url = page.find('"', start_url+1)
-
-# logging
-print ('end_url: ', end_url)
-
-url = page[start_url+1:end_url]
+url, end_url = get_next_url(page)
 
 # logging
 print (url)
+print (end_url)
+
+page = page[end_url:]
+
+
 
 
 
