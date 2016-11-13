f6be0ca3cdca6af2c0dc9de73567b550036be7fa
hellobasti <bastianwinterott@gmail.com>
2016-11-13 15:34:35 +0100

    Search all links until no more links found

--- web_crawler.py
+++ web_crawler.py
@@ -3,29 +3,44 @@ const_html_tag = '<a href='
 
 def get_next_url(page):
 
+    print (page)
+
     start_html_tag = page.find(const_html_tag)
 
+    # Link in the website not found
+    if (start_html_tag == -1):
+        return None, 0
+
     start_url = page.find('"', start_html_tag)
-
+    print ('start_url: ', start_url)
     end_url = page.find('"', start_url+1)
-
+    print ('end_url: ', end_url)    
     url = page[start_url+1:end_url]
 
     return url, end_url
+
+def get_all_urls(page):
+
+    while True:       
+        url, end_url = get_next_url(page)
+
+        print ('url', url)
+        
+        if url:
+            page = page[end_url:]
+        
+        else:
+            break
 
 # Select a url for a website
 # Request
 # Handle the response --> result page
 
-page = ('<div id="top_bin"> <div id="top_content" class="width960"> <div class="udacity float-left"> <a href="http://udacity.com">Hello world</a>')
+page = ('<div id="top_bin"> <div id="top_content" class="width960"> <div class="udacity float-left"> <a href="http://udacity.com">Hello world</a><div id="top_bin"> <div id="top_content" class="width960"> <div class="udacity float-left"> <a href="http://udacity.com">Hello world</a><div id="top_bin"> <div id="top_content" class="width960"> <div class="udacity float-left"> <a href="http://udacity.com">Hello world</a><div id="top_bin"> <div id="top_content" class="width960"> <div class="udacity float-left"> <a href="http://udacity.com">Hello world</a><div id="top_bin"> <div id="top_content" class="width960"> <div class="udacity float-left"> <a href="http://udacity.com">Hello world</a>')
 
-url, end_url = get_next_url(page)
+get_all_urls(page)
 
-# logging
-print (url)
-print (end_url)
 
-page = page[end_url:]
 
 
 
